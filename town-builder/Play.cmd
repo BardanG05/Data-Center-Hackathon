d3f66cbfc3723d959d405470458f4d410de01c3b
BardanG05 <bardang05@gmail.com>
2026-09-23 02:15:33 +0100

    Delete town-builder directory

--- town-builder/Play.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set "GODOT=%~dp0..\.tools\godot\Godot_v4.7.2-stable_win64.exe"
-if not exist "%GODOT%" (
-  echo Open project.godot in the standard Godot 4 editor and press F5.
-  pause
-  exit /b 1
-)
-start "" "%GODOT%" --path "%~dp0."
